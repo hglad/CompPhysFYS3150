ab48ea0527edaa6fa274c90e9934ba77ecf38e07
hglad <hanseglad@gmail.com>
2018-09-03 16:42:55 +0200

    Updated Python code for project 1, code runs and produces results

--- project1.py
+++ project1.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100
+n = 1000
 x = np.linspace(0,1,n)
 h = (x[-1] - x[0])/float(n)
+print h
 
 # Vectors representing matrix A
 a = np.ones(n-1)*(-1)
@@ -13,30 +14,27 @@ c = np.ones(n-1)*(-1)
 v = np.zeros(n)
 f = np.zeros(n) # Discretization of f(x)
 
-def func_f(x):
+def func_f(x):  #f_tilde (b_tilde in project text)
     return h**2*100*np.exp(-10*x)
 
 def u(x):       # Analytical solution
     return 1 - (1-np.exp(-10))*x - np.exp(-10*x)
 
-# Row reduction using vectors only
-"""
-for i in range(0, n-1):
-    a[i] -= a[i]/b[i-1] * b[i-1]
-    b[i+1] -= a[i]/b[i] * c[i]
-"""
-f_tilde = np.zeros(n)
-b_tilde = np.zeros(n)
 f = func_f(x)
-for i in range(1, n-1):
-    f_tilde[i] = f[i] - a[i-1]/b[i-1] * f[i-1]
-    b_tilde[i] = b[i] - a[i-1]*c[i-1]/b[i-1]
+"""
+Set f_tilde and b_tilde to f and b respectively, so that the first step can be calculated properly (since the first step does not use tilde values).
+"""
+f_tilde = f
+b_tilde = b
+for i in range(1, n):
+    b_tilde[i] = b[i] - a[i-1]/b_tilde[i-1] * c[i-1]
+    f_tilde[i] = f[i] - a[i-1]/b_tilde[i-1] * f_tilde[i-1]
 
-for i in range(1, n-1):
-    i = n-1-i
-    #print (i)
-    v[i] = f_tilde[i]/(c[i-1]+b_tilde[i])
+for i in range(n-2, 0, -1):
+    v[i] = (f_tilde[i] - v[i+1]*c[i])/b_tilde[i]
 
+print (v)
+print (u(x))
 plt.plot(x, v)
 plt.plot(x, u(x))
 plt.legend(['Numerical', 'Analytical'])
