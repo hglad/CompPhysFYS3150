5263f2b51cac21944cfa0abd22dd1edcf791b36a
hglad <hanseglad@gmail.com>
2018-09-05 14:27:52 +0200

    Added code for task c). Also written code to time the main algorithm in b) and c).

---
+++ project1/project1c.cpp
@@ -0,0 +1,110 @@
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+
+/*
+Task c) With identical values along the diagonal, we can set all elements in vectors a and c to -1.
+*/
+
+//f_mark = h**2 f
+inline double f_mark__(double x, double h) {return pow(h,2)*100.0*exp(-10.0*x);}
+inline double u__(double x)	{return (1 - (1 - exp(-10))*x - exp(-10*x));}
+/*
+Function that creates an array of size 'm' and allocates memory for indices of type double
+*/
+double* vec_mem(int m)
+{
+	double *v = new double[m];
+	return v;
+}
+/*
+Integer version of function above
+*/
+int* vec_mem_int(int m)
+{
+	int *v = new int[m];
+	return v;
+}
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+		// Initialize variables to calculate execution time
+		clock_t t;
+		t = clock();
+
+		int n=atoi(argv[1]);		   // endpoint of x-array
+		double h = 1. / (n+1);
+
+		// Allocate memory to arrays used
+		double *x, *f_mark, *u, *b, *v, *f_tilde, *b_tilde;
+		int *a, *c;
+		x = vec_mem(n+2); f_mark = vec_mem(n+2); u = vec_mem(n+2);
+		b = vec_mem(n+2); f_tilde = vec_mem(n+2); b_tilde = vec_mem(n+2);
+		a = vec_mem_int(n+1); c = vec_mem_int(n+1);		// lower and upper diagonals
+		v = vec_mem(n+2);															// unknown solution
+
+		for (int i=0; i < n+2 ; i++)		// create x-array
+		{
+			x[i] = h * i;
+			f_mark[i] = f_mark__(x[i], h);
+			u[i] = u__(x[i]);						// analytical solution
+		//	printf ("x = %g, f_mark = %g \n", x[i], f_mark[i]);
+		}
+		// Set boundary conditions
+		v[0] = 0;
+		v[n+1] = 0;
+
+		// Fill main diagonal
+		for (int i=0; i < n+2; i++)
+		{
+			b[i] = 2;
+		}
+
+		// Set initial f_tilde and b_tilde values to initial f and b values, since
+		// tilde values are not used until index i = 1
+		f_tilde[0] = f_mark[0];
+		f_tilde[1] = f_mark[1];
+		b_tilde[0] = b[0];
+		b_tilde[1] = b[0];
+
+		// Main algorithm: perform forward and backward substitution
+		double _b_tilde;
+		for (int i=2; i < n+2; i++)
+		{
+			_b_tilde = 1./(b_tilde[i-1]);	// save FLOPS by calculating once
+			b_tilde[i] = b[i] - _b_tilde;
+	    f_tilde[i] = f_mark[i] + f_tilde[i-1]*_b_tilde;
+		}
+		for (int i=n; i > 0; i--)
+		{
+			v[i] = (f_tilde[i] + v[i+1])/b_tilde[i];
+		}
+
+		// Calculate time by using number of clock ticks elapsed
+		t = clock() - t;
+		double sec;
+		sec = (float)t/CLOCKS_PER_SEC;	// num. of seconds algorithm takes to run
+		cout << sec << endl;
+
+		// Write arrays x, u and v to file
+		ofstream myfile;
+		char *project1_c_data;
+		myfile.open ("project1_c_data.txt");
+
+		for (int i=0; i < n+2; i++)
+		{
+		//	cout << i << ' ' << b_tilde[i] << endl;
+		//	printf ("%g %g %g\n", x[i], f_tilde[i], v[i]);
+			myfile << x[i] << ' ' << u[i] << ' ' << v[i] << endl;
+		}
+		cout << v[n+1] << endl;
+		myfile.close();
+		printf ("Solution computed for n = %i. Results written to file.\n", n);
+
+		return 0;
+}
