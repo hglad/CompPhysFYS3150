db4177363304df3df9d25f6daebd6b2977e056fa
hglad <hanseglad@gmail.com>
2018-09-06 15:19:38 +0200

    Updated code to include endpoints. Also defined m=n+2 to make indexing more readable (hopefully)

--- project1/project1e.cpp
+++ project1/project1e.cpp
@@ -14,34 +14,31 @@ inline double u__(double x)	{return (1 - (1 - exp(-10))*x - exp(-10*x));}
 int main(int argc, char* argv[])
 {
   int n=atoi(argv[1]);
+  int m = n+2;        // define new variable to (maybe) make code more readable
+  mat A = zeros(m,m);   // LHS matrix
   double h = 1./(n+1);
-  mat A = zeros(n,n);   // LHS matrix
 
-/* Iterate through rows and set all diagonal elements up to n-3. Last index i = n is not iterated over since i+1 will go out of bounds at this point. */
-  for (int i=0; i < n-1; i++)
+/* Iterate through rows and fill diagonals. The endpoints i = 0 and i = m-1 are not filled since these represent the boundaries. */
+  for (int i=1; i < m-2; i++)
   {
     A(i, i)    =  2;      // Main diagonal
     A(i, i+1)  = -1;      // Upper diagonal values
     A(i+1, i)  = -1;      // Lower diagonal values
   }
-// Set last diagonal elements
-  A(n-1, n-1) = 2;
+// Set 2nd last main diagonal manually
+  A(m-2, m-2) = 2;
+  cout << A << endl;
 
-  mat f_mark = zeros(n);
-  double *x = new double[n];
-  double *u = new double[n];
+  mat f_mark = zeros(m);    // m x 1 matrix (vector)
+  double *x = new double[m];
+  double *u = new double[m];
 
-// Compute values at endpoints
-  x[0] = h;
-  f_mark(0) = f_mark__(x[0], h);
-  x[n-1] = x[0]+(n-1)*h;
-  f_mark(n-1) = f_mark__(x[n-1], h);
-
-  for (int i=1; i < n-1 ; i++)		// create x-array and compute RHS
+  for (int i=0; i < m ; i++)		// create x-array and compute RHS
   {
-    x[i] = x[i-1] + h;
+    x[i] = h * i;
     f_mark(i) = f_mark__(x[i],h);
   }
+
 // Solve using LU decomposition
   vec v = solve(A, f_mark);
 
@@ -49,10 +46,11 @@ int main(int argc, char* argv[])
   ofstream myfile;
   char *project1_e_data;
   myfile.open ("project1_e_data.txt");
-  for (int i=0; i < n; i++)
+  for (int i=0; i < m; i++)
   {
     u[i] = u__(x[i]);						// analytical solution
     myfile << x[i] << ' ' << u[i] << ' ' << v[i] << endl;
+    printf ("%g %g %g\n", x[i], u[i], v[i]);
   }
 
   myfile.close();
