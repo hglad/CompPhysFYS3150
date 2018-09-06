ac1b585fa59608f3c0412f17179be321ac01b97d
hglad <hanseglad@gmail.com>
2018-09-06 16:15:43 +0200

    Removed calculation at boundaries

--- project1/project1e.cpp
+++ project1/project1e.cpp
@@ -14,28 +14,30 @@ inline double u__(double x)	{return (1 - (1 - exp(-10))*x - exp(-10*x));}
 int main(int argc, char* argv[])
 {
   int n=atoi(argv[1]);
-  int m = n+2;        // define new variable to (maybe) make code more readable
-  mat A = zeros(m,m);   // LHS matrix
+  mat A = zeros(n,n);   // LHS matrix
   double h = 1./(n+1);
 
-/* Iterate through rows and fill diagonals. The endpoints i = 0 and i = m-1 are not filled since these represent the boundaries. */
-  for (int i=1; i < m-2; i++)
+// Iterate through rows filling fill main diagonal and adjacent values
+  for (int i=0; i < n-1; i++)
   {
     A(i, i)    =  2;      // Main diagonal
     A(i, i+1)  = -1;      // Upper diagonal values
     A(i+1, i)  = -1;      // Lower diagonal values
   }
+  
 // Set 2nd last main diagonal manually
-  A(m-2, m-2) = 2;
+  A(n-1, n-1) = 2;
   cout << A << endl;
 
-  mat f_mark = zeros(m);    // m x 1 matrix (vector)
-  double *x = new double[m];
-  double *u = new double[m];
+  mat f_mark = zeros(n);    // m x 1 matrix (vector)
+  double *x = new double[n];
+  double *u = new double[n];
 
-  for (int i=0; i < m ; i++)		// create x-array and compute RHS
+  x[0] = h;
+  f_mark(0) = f_mark__(x[0], h);
+  for (int i=1; i < n ; i++)		// create x-array and compute RHS
   {
-    x[i] = h * i;
+    x[i] = x[i-1] + h;
     f_mark(i) = f_mark__(x[i],h);
   }
 
@@ -46,7 +48,7 @@ int main(int argc, char* argv[])
   ofstream myfile;
   char *project1_e_data;
   myfile.open ("project1_e_data.txt");
-  for (int i=0; i < m; i++)
+  for (int i=0; i < n; i++)
   {
     u[i] = u__(x[i]);						// analytical solution
     myfile << x[i] << ' ' << u[i] << ' ' << v[i] << endl;
