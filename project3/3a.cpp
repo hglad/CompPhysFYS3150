f28471aa6b2155313993a20456c373bb627392ba
Hans <hanseglad@gmail.com>
2018-10-13 18:29:03 +0200

    Changed structure of arrays and attempted to create class

--- project3/3a.cpp
+++ project3/3a.cpp
@@ -10,42 +10,42 @@ int main(int argc, char* argv[])
 {
   double T = 1;   // simulation time in years
   double dt = 0.001;
-  int n = (T/dt);
+
+  int n = (T/dt) +1; // without +1: planet does not reach starting point
   cout << n << endl;
 
-  double pi = M_PI;       // from math.h
+  double pi = M_PI;       // define using math library
   double GM = 4*pi*pi;
 
   double M_Earth_kg = 6*pow(10, 24);
   double M_Sun_kg = 2*pow(10, 30);
-  double M_Sun = 1;
+  double M_Sun = 1;       // use solar mass as scaling factor
   double M_Earth = M_Earth_kg / M_Sun_kg;
 
   // Define vectors for physical parameters
-  // with zeros(n): planet does not reach starting point
-  vec x = zeros(n+1);
-  vec y = zeros(n+1);
-  vec v_x = zeros(n+1);
-  vec v_y = zeros(n+1);
-  vec a_x = zeros(n+1);
-  vec a_y = zeros(n+1);
-  x(0) = 1;     // AU
-  y(0) = 0;     // AU
-  v_y(0) = 2*pi*x(0);   // set initial velocity to 2*pi*r in y-direction
+  mat s = zeros(n, 2);
+  mat v = zeros(n, 2);
+  mat a = zeros(n, 2);
 
-  double F_0 = GM*M_Earth/(x(0)*x(0));
-  a_x(0) = -F_0/M_Earth * x(0);
-  a_y(0) = -F_0/M_Earth * y(0);
+  s(0,0) = 1;     // AU
+  s(0,1) = 0;     // AU
+
+  v(0,0) = 0;
+  v(0,1) = 2*pi*s(0, 0);   // set initial velocity to 2*pi*r in y-direction
+
+  double r_0 = sqrt(s(0,0)*s(0,0) + s(0,1)*s(0,1));
+  double F_0 = GM*M_Earth/(r_0*r_0);
+  a(0,0) = -F_0/M_Earth * s(0,0);
+  a(0,1) = -F_0/M_Earth * s(0,1);
 
   ofstream myfile;
-  char *project3a;
   myfile.open ("project3a.txt");
-  myfile << x(0) << ' ' << y(0) << endl;
+  myfile << s(0,0) << ' ' << s(0,1) << endl;
   // Euler
   /*
-  for (int i=0; i < n; i++)
+  for (int i=0; i < n-1; i++)
   {
-      double r = sqrt(x(i)*x(i) + y(i)*y(i));
+      double r = sqrt(x(i)*x(i) + y(i)*yaa(i));
       double F = GM*M_Earth/(r*r);
       a_x(i+1) = -F/M_Earth * x(i);
       a_y(i+1) = -F/M_Earth * y(i);
@@ -59,20 +59,20 @@ int main(int argc, char* argv[])
   }
   */
   // Verlet
-  for (int i=0; i < n; i++)
+  for (int i=0; i < n-1; i++)
   {
-      x(i+1) = x(i) + dt*v_x(i) + a_x(i)*dt*dt/2;
-      y(i+1) = y(i) + dt*v_y(i) + a_y(i)*dt*dt/2;
+      s(i+1, 0) = s(i, 0) + dt*v(i, 0) + a(i, 0)*dt*dt/2;
+      s(i+1, 1) = s(i, 1) + dt*v(i, 1) + a(i, 1)*dt*dt/2;
 
-      double r = sqrt(x(i)*x(i) + y(i)*y(i)); // distance Earth-Sun (should always be 1 AU)
+      double r = sqrt(s(i, 0)*s(i, 0) + s(i, 1)*s(i, 1)); // distance Earth-Sun (should always be 1 AU)
       double F = GM*M_Earth/(r*r);    // magnitude of grav. force
-      a_x(i+1) = -F/M_Earth * x(i+1);
-      a_y(i+1) = -F/M_Earth * y(i+1);
+      a(i+1, 0) = -F/M_Earth * s(i+1, 0);     // cos(theta) = x if radius is 1 AU
+      a(i+1, 1) = -F/M_Earth * s(i+1, 1);
 
-      v_x(i+1) = v_x(i) + dt/2*(a_x(i+1)+a_x(i));
-      v_y(i+1) = v_y(i) + dt/2*(a_y(i+1)+a_y(i));
+      v(i+1, 0) = v(i, 0) + dt/2*(a(i+1, 0)+a(i, 0));
+      v(i+1, 1) = v(i, 1) + dt/2*(a(i+1, 1)+a(i, 1));
 
-      myfile << x(i+1) << ' ' << y(i+1) << endl;
+      myfile << s(i+1, 0) << ' ' << s(i+1, 1) << endl;
   }
 
   myfile.close();
