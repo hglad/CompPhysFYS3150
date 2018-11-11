8992f8ccb38618e4cf906b6d0210c26cc5d16ac5
hglad <hanseglad@gmail.com>
2018-11-11 17:22:41 +0100

    Added expectation values, changed to more general methods

--- project4/main_ising.cpp
+++ project4/main_ising.cpp
@@ -11,101 +11,124 @@ using namespace arma;
 
 inline double energy_calc(imat S) {return -(2*S(0)*S(1) + 2*S(0)*S(2) + 2*S(1)*S(3) + 2*S(2)*S(3));}
 
-void newPos(int *x, int *y, int L)
+inline int PBC(int index, int max, int add) {return (index + max + add) % max ;}
+
+void init_params(imat S, double &E, double &M)
 {
-  *x = rand() % L;
-  *y = rand() % L;
-  return;
+  int L = S.n_cols;
+  for (int x=0; x < L; x++)
+  {
+    for (int y=0; y < L; y++)
+    {
+      M += S(x, y);
+      E -= S(x, y)*( S(PBC(x, L, -1), y) + S(x, PBC(y, L, -1)));
+    }
+  }
 }
 
-void flip(imat& S)        // function to flip a random spin
+void newPos(int &x, int &y, int L)
 {
-  int n = S.n_elem;
-  int rand_pos = rand() % n;
-  S(rand_pos) *= -1;      // changing sign --> flipping spin
+  x = rand() % L;
+  y = rand() % L;
   return;
 }
 
 map<double, double> transitions(double T)
 {
-  map<double, double> acceptAmp; // similar to python dictionary
+  map<double, double> possible_E; // similar to python dictionary
 
   for (int dE = -8; dE <= 8; dE += 4)
   {
     // dictionary element where dE is the key to the corresponding energy
-    acceptAmp.insert(pair<double, double>(dE, exp(-1/T*(dE))));
+    possible_E.insert(pair<double, double>(dE, exp(-1/T*(dE))));
   }
 
-  return acceptAmp;
+  return possible_E;
 }
 
 int main(int argc, char const *argv[])
 {
-  int numMC = 10000;     // num. of MC-cycles
+  int numMC = 100000;     // num. of MC-cycles
   int L = 2;  int n = L*L;
-  int temp_spin;
+  //int temp_spin;
   double T = 1;
 
   imat S(L,L);
-  vec E = zeros(numMC);
-  vec Cvs = zeros(numMC);
-
-  double dE, Cv, M, new_E, w, r;
-  int x, y, rand_pos;
+  double dE, r, M;
+  int x, y;
 
   random_device rd;  //Will be used to obtain a seed for the random number engine
-  mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-  uniform_real_distribution<> dist(0, 1);
+  mt19937_64 gen(10); //Standard mersenne_twister_engine seeded with rd()
+  //mt19937_64 gen(rd());
+  uniform_real_distribution<double> dist(0, 1);
+  uniform_int_distribution<int> RNGpos(0, L-1);
 
   // Generate L*L matrix with spins -1 or 1
   srand (time(NULL));
   for (int i=0; i < n; i++)
   {
     S(i) = rand() % 2;  // returns 0 or 1, corresponding to spin -1 and 1
-    if (temp_spin == 0)
+    if (S(i) == 0)
     {
       S(i) = -1;
     }
   }
-
-  cout << S << endl;
+  S.fill(1);
 
   // Initial energy
-  double E_0 = energy_calc(S);
-  E(0) = E_0;
+  double E_0 = 0;
+  double magmom = 0;
+  init_params(S, E_0, magmom);
+  cout << E_0 << endl;
+  double energy = E_0;
 
   ofstream myfile;
   myfile.open ("ising_data.txt");
+  myfile << E_0 << ' ' << magmom << endl;
 
-  for (int i=1; i < numMC; i++)
+  int counter;
+  map<double, double> w = transitions(T);
+
+  for (int k=1; k < numMC; k++)
   {
-    flip(S);
-    E(i) = energy_calc(S);
-    dE = E(i) - E(i-1);
-
-    // Metropolis algorithm
-    if (dE > 0)
+    for (int i = 0; i < L; i++)
     {
-      // compare w with random number r
-      w = exp(-dE/T);
-      r = dist(gen);
-
-      // keep new configuration if true
-      if (r <= w)
+      for (int j = 0; j < L; j++)
       {
-        E(i) = E(i);
-      }
-      else
-      {
-        E(i) = E(i-1);
-      }
+      //  newPos(x, y, L);   // new random position in lattice
+        x = RNGpos(gen);
+        y = RNGpos(gen);
+        dE = 2*S(x, y) * (S(x, PBC(y, L, -1)) + S(PBC(x, L, -1), y) + S(x, PBC(y, L, 1)) + S(PBC(x, L, 1), y));
 
+        // Metropolis algorithm
+        // compare w with random number r
+        r = dist(gen);
+        if (r <= w.find(dE)->second)
+        {
+          counter += 1;
+          S(x, y) *= -1.;           // flip spin
+          energy += dE;
+          cout << energy << endl;
+          magmom += S(x, y);
+          }
+      }
     }
 
-    myfile << E(i) << ' ' << E(i) << endl;
+    myfile << energy << ' ' << fabs(magmom) << endl;
   }
+  double E, E2, M2, absM, C_V, chi;
+  E = energy/numMC;
+  E2 = energy*energy/numMC;
+  M = magmom/numMC;
+  absM = abs(magmom)/numMC;
+  M2 = magmom*magmom/numMC;
 
-  cout << S << endl;
+  C_V = (E2 - E*E)/(pow(T,2)*pow(L,2));
+  chi = (M2 - M*M)/(T*pow(L,2));
+
+  //cout << counter << endl;
+  cout << "---" << endl;
+  cout << E << ' ' << M << ' ' << M2 << ' ' << C_V << ' ' << chi << endl;
 
   myfile.close();
 
@@ -114,26 +137,11 @@ int main(int argc, char const *argv[])
 }
 
 /*
-flip(S);
-
-new_E = energy(S);
-dE = new_E - E;
-
-if (dE <= 0)
-{
-  E = new_E;
-}
-
-else
-{
-  // compare w with random number r
-  w = exp(-dE/T);
-  r = dis(gen);
-  // keep new configuration if true
-  if (r <= w)
+  void flip(imat& S)        // function to flip a random spin
   {
-    E = new_E;
+    int n = S.n_elem;
+    int rand_pos = rand() % n;
+    S(rand_pos) *= -1;      // changing sign --> flipping spin
+    return;
   }
-
-}
 */
--- project4/plot_ising.py
+++ project4/plot_ising.py
@@ -2,15 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 infile = open("ising_data.txt", 'r')
-y = []
-
+E = []
+M = []
+T = 1.
 for line in infile:
     cols = line.split(' ')
-    y.append(float(cols[0]))
+    E.append(float(cols[0]))
+    M.append(float(cols[1]))
 
-x = np.linspace(0, len(y)-1, len(y))        # MC-cycles
+x = np.linspace(0, len(E)-1, len(E))        # MC-cycles
 
-plt.plot(x, y)
+plt.plot(x, E)
+plt.grid('on'); plt.ylabel('E')
+plt.figure()
 
-plt.grid('on')
+plt.plot(x, M)
+plt.grid('on'); plt.ylabel('M')
+
+E_ = -np.sinh(8)*8 / (3 + np.cosh(8))
+Cv_ = T**(-2) * (64*np.cosh(8)*(3 + np.cosh(8)) - 8*np.sinh(8)*8*np.sinh(8)) / (3 + np.cosh(8))**2
+M2_ = (8*np.exp(8) + 8)/(3 + np.cosh(8))
+absM_ = (2*np.exp(8) + 4)/(3 + np.cosh(8))
+chi_ = M2_/T
+
+print E_, absM_, M2_, Cv_, chi_
+
 plt.show()
